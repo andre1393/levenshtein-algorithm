40dd1f0dabe83d4408c3b85924a79100f46581a6
linux <amdre_1393@hotmail.com>
2019-12-07 19:57:16 -0800

    challenge completed

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
 
 	public static void main(String[] args) {
+		final int maxDistance = 2;
+
+		System.out.println("Exercises 1/2: \n");
+		runLevenshteinTests();
+		System.out.println("-----------------------------------------");
+		System.out.println("Exercises 3/4: \n");
+		runLevenshteinWithEarlyTests(maxDistance);
+		System.out.println("-----------------------------------------");
+		System.out.println("Exercise 5: \n");
+		long test1 = runLevenshteinPerformanceTests(10000);
+		System.out.println("\n");
+		long test2 = runLevenshteinWithEarlyExitPerformanceTests(10000, maxDistance);
+		System.out.printf("\nLeveshtein with early exit was %d nano seconds faster than the regular one.\n", test1-test2);
+	}
+
+	// exercise 2
+	public static void runLevenshteinTests() {
 		final int n = 4;
 
 		String[] tokens1 = new String[]{"Haus", "Haus", "Haus", "Kartoffelsalat"};
@@ -8,8 +28,71 @@ public class Main {
 		int[] distances = new int[]{1, 2, 3, 12};
 
 		Solution solution = new Solution();
+
 		for(int i=0; i<n; i++) {
 			System.out.printf("lev(%s, %s) = %d (expected = %d)\n", tokens1[i], tokens2[i], solution.levenshtein(tokens1[i], tokens2[i]), distances[i]);
 		}
+	}
+
+	// exercise 4
+	public static void runLevenshteinWithEarlyTests(int maxDistance) {
+	    final int n = 4;
+
+		String[] tokens1 = new String[]{"Haus", "Haus", "Haus", "Kartoffelsalat"};
+		String[] tokens2 = new String[]{"Maus", "Mausi", "Häuser", "Runkelrüben"};
+		int[] distances = new int[]{1, 2, 3, 3};
+
+		Solution solution = new Solution();
+		for(int i=0; i<n; i++) {
+			System.out.printf("lev(%s, %s) = %d (expected = %d)\n", tokens1[i], tokens2[i], solution.levenshtein(tokens1[i], tokens2[i], maxDistance), distances[i]);
+		}
+	}
+
+	public static long runLevenshteinPerformanceTests(int nTests) {
+		final int n = 4;
+
+		String[] tokens1 = new String[]{"Haus", "Haus", "Haus", "Kartoffelsalat"};
+		String[] tokens2 = new String[]{"Maus", "Mausi", "Häuser", "Runkelrüben"};
+
+		Solution solution = new Solution();
+
+		System.out.println("performance test of levenshtein:");
+		
+		long startTest = System.nanoTime();
+
+		for(int t=0; t<nTests; t++){
+			for(int i=0; i<n; i++) {
+				solution.levenshtein(tokens1[i], tokens2[i]);
+			}
+		}
+
+		long elapsedTime = System.nanoTime()-startTest;
+		System.out.printf("time elapsed to process all words pairs: %d nano seconds\n", elapsedTime);
+
+		return elapsedTime;
+	}
+
+	public static long runLevenshteinWithEarlyExitPerformanceTests(int nTests, int maxDistance) {
+		final int n = 4;
+
+		String[] tokens1 = new String[]{"Haus", "Haus", "Haus", "Kartoffelsalat"};
+		String[] tokens2 = new String[]{"Maus", "Mausi", "Häuser", "Runkelrüben"};
+
+		Solution solution = new Solution();
+
+		System.out.println("performance test of levenshtein with early exit:");
+		
+		long startTest = System.nanoTime();
+		
+		for(int t=0; t<nTests; t++) {
+			for(int i=0; i<n; i++) {
+				solution.levenshtein(tokens1[i], tokens2[i], maxDistance);
+			}
+		}
+
+		long elapsedTime = System.nanoTime()-startTest;
+		System.out.printf("time elapsed to process all words pairs: %d nano seconds\n", elapsedTime);
+		
+		return elapsedTime;
 	}
 }
--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 
 public class Solution {
 
+	// exercise 1
 	public int levenshtein(String token1, String token2) {
 		final int lenToken1 = token1.length();
 		final int lenToken2 = token2.length();
@@ -35,6 +36,48 @@ public class Solution {
 
 				column[i] = newValue;
 			}
+        }
+
+		return column[lenToken1];
+	}
+
+	// exercise 3
+	public int levenshtein(String token1, String token2, int maxDist) {
+		final int lenToken1 = token1.length();
+		final int lenToken2 = token2.length();
+       
+		int cost;
+
+		// allocate first column
+		int[] column = new int[lenToken1+1];
+
+		// initialize fill first column
+		for(int i=0; i<=lenToken1; i++) {
+			column[i] = i;
+		}
+
+		// iterate over columns
+        for(int j=1; j<=lenToken2; j++) {
+        	int oldValue = j-1; // value to replaced
+        	int newValue = j; // value replaced
+
+        	// iterate over rows
+			for(int i=1; i<=lenToken1; i++) {
+			column[i-1] = newValue;
+
+				if(token1.charAt(i-1) == token2.charAt(j-1)) cost = 0;
+				else cost = 1;
+
+				newValue = this.min(column[i] + 1, column[i-1] + 1, oldValue + cost);
+				
+				// before replacing new value, save the old one
+				oldValue = column[i];
+
+				column[i] = newValue;
+			}
+
+			if(column[Math.min(lenToken1, j)] > maxDist)
+				return maxDist + 1;
         }
 
 		return column[lenToken1];
